4dc6456767a5a5dadb03e4b4b1904e8a4e94ee7a
Auez <kemelovauez4@gmail.com>
2026-09-23 23:33:54 +0500

    change api front

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
-      'https://planda-seven.vercel.app',
+      'https://planda-front.vercel.app',
     ],
     credentials: true,
   });
