e1461fe2fee03dec56c591386f3a61d45e6f4cd5
Auez <kemelovauez4@gmail.com>
2026-09-25 19:51:58 +0500

    add token lifecycle 24h

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { GqlAuthGuard } from './gql-auth.guard.js';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   providers: [AuthService, AuthResolver, GoogleAuthService, GqlAuthGuard],
