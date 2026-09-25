eac01eccc3a3623badda24e983ce51586e29e07c
Auez <kemelovauez4@gmail.com>
2026-09-25 18:28:25 +0500

    change file auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,3 +1,40 @@
+// import {
+//   CanActivate,
+//   ExecutionContext,
+//   Injectable,
+//   UnauthorizedException,
+// } from '@nestjs/common';
+// import { JwtService } from '@nestjs/jwt';
+// import { Request } from 'express';
+
+// @Injectable()
+// export class AuthGuard implements CanActivate {
+//   constructor(private readonly jwtService: JwtService) {}
+
+//   async canActivate(context: ExecutionContext): Promise<boolean> {
+//     const request = context.switchToHttp().getRequest<Request>();
+
+//     const token = this.extractToken(request);
+
+//     if (!token) {
+//       throw new UnauthorizedException();
+//     }
+
+//     try {
+//       const payload = await this.jwtService.verifyAsync(token);
+//       request['user'] = payload;
+//     } catch {
+//       throw new UnauthorizedException();
+//     }
+
+//     return true;
+//   }
+
+//   private extractToken(request: Request): string | undefined {
+//     return request.cookies?.access_token;
+//   }
+// }
+
 import {
   CanActivate,
   ExecutionContext,
@@ -22,6 +59,7 @@ export class AuthGuard implements CanActivate {
 
     try {
       const payload = await this.jwtService.verifyAsync(token);
+
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
@@ -31,6 +69,18 @@ export class AuthGuard implements CanActivate {
   }
 
   private extractToken(request: Request): string | undefined {
-    return request.cookies?.access_token;
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      return undefined;
+    }
+
+    const [type, token] = authHeader.split(' ');
+
+    if (type !== 'Bearer' || !token) {
+      return undefined;
+    }
+
+    return token;
   }
 }
